d40cf6ef4a4455ff45c37d0d9b8a88fecc16b8f3
karthikthella <karthikthella@gmail.com>
2025-09-29 16:36:43 +0530

    Message type chey mannad

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -1,11 +1,11 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const PORT = process.env.PORT || 5000;
-export const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:5173';
-export const MONGO_URI = process.env.MONGO_URI || '';
+export const PORT = process.env.PORT;
+export const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN;
+export const MONGO_URI = process.env.MONGO_URI;
 
-export const JWT_SECRET = process.env.JWT_SECRET || 'p0tat0';
-export const JWT_EXP = process.env.JWT_EXP || '7d';
+export const JWT_SECRET = process.env.JWT_SECRET;
+export const JWT_EXP = process.env.JWT_EXP;
 
 
